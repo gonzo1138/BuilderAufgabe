a3808327892665242e1bc98a8eaf7a77778fb8fa
Thomas <gonzo113@bassfreaks.de>
2021-03-15 10:09:08 +0100

    Initial commit

---
+++ src/HTMLpooper.java
@@ -0,0 +1,18 @@
+/*
+    1. Program das komplette Webseiten bauen kann (Builder Pattern)
+    2. es sollen alle Elemente korrekt eingebunden werden
+    3. Man soll im Programm ein Menu haben mit dem man Elemente hinzufügen kann
+    (Model View Controller Pattern)
+
+    4. Die Daten der Webseite sollen als Singleton realisert werden
+    5. BONUS : Die Webseite soll als html Datei gespeichert werden können
+    Hilfe : Schaut euch die Java beispiele zu Builder und MVC an
+    Tip : 1 und 4 ergeben probleme zusammen
+*/
+
+public class HTMLpooper {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
